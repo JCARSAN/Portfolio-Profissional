b5e1963ff96d01283c790f3aeb03a6464b50ce51
unknown <jcarsan1000@gmail.com>
2022-10-05 02:39:17 -0300

    Consertando erros de responsividade nas Section Portfólio e Contato

--- src/components/Main/Contato/index.tsx
+++ src/components/Main/Contato/index.tsx
@@ -14,14 +14,14 @@ export function Contato({scrollPosition} : ContactProps){
     }
 
     return(
-        <VStack as="section" id="contato" width="100%" alignItems={{base:"center", md:"flex-start"}} padding={{base:'64px 25px'}}>
+        <VStack as="section" id="contato" width="100%" alignItems={{base:"center", md:"flex-start"}} padding={{base:'64px 10px'}}>
             <Heading as="h2">Contato</Heading>
             <VStack alignItems="flex-start" width="100%" maxWidth={1440} overflow="hidden">
                 <VStack width="100%" maxWidth={1440} spacing={5} marginLeft={animated?'0':'-100%'} className={animated? 'show-element' : ''} alignItems="flex-start">
                     <Text fontSize="1.25rem" marginBottom="64px">Deseja construir um site ou um app? Entre em contato comigo!</Text>
-                    <HStack width="100%" alignItems="flex-start" spacing={10}>
+                    <HStack width="100%" alignItems="flex-start" spacing={{base:2, md:5}}>
                         <Image src="/images/iphone-edit.png" width={100}/>
-                        <VStack alignItems="flex-start" lineHeight={2} fontSize="1.25rem">
+                        <VStack alignItems={{base:'center',md:"flex-start"}} lineHeight={2} fontSize={{base:'1rem', md:"1.25rem"}}>
                             <Text>E-mail: jcarsan1000@gmail.com</Text>
                             <Text>Whatsapp: (21)99205-6323</Text>
                             <HStack fontSize="2rem" width="100%" spacing={30}>
--- src/components/Main/Portfolio/index.tsx
+++ src/components/Main/Portfolio/index.tsx
@@ -7,8 +7,6 @@ type PortFolioProps = {
 }
 
 export function Portfolio({ scrollPosition } : PortFolioProps){
-
-    console.log('Portfólio: ',scrollPosition);
     const [animated,setAnimated] = useState(false);
 
     if(!animated && scrollPosition >= 1800){
@@ -17,9 +15,10 @@ export function Portfolio({ scrollPosition } : PortFolioProps){
 
     return(
         <VStack as="section" id="portfolio" alignItems={{base:"center", md:"flex-start"}}>
-            <Heading as="h2" marginLeft={{base:"8px"}}>Portfólio</Heading>
-            <Flex overflow="hidden" width="90%">
-                <SimpleGrid columns={{lg:3,md:1}} spacing={10} marginLeft={animated? 0 : '-500%'} className={animated? 'grow-up' : ''}>
+            <Heading as="h2">Portfólio</Heading>
+            <Flex overflow="hidden">
+                
+                <SimpleGrid columns={{lg:3,md:1}} spacing={10} marginLeft={animated? 0 : '-500%'} className={animated? 'grow-up' : ''} margin="auto" width={{base:'90%',lg:'100%'}}>
                     <PortfolioWork 
                         title={"VideoConferênciaApp"} 
                         src={"/images/videoconferenciaapp.png"} 
@@ -39,6 +38,27 @@ export function Portfolio({ scrollPosition } : PortFolioProps){
                         linkRepository={"https://github.com/JCARSAN/blog-react-rocketseat"} 
                     />
                 </SimpleGrid>
+                
+                {/*
+                <PortfolioWork 
+                        title={"VideoConferênciaApp"} 
+                        src={"/images/videoconferenciaapp.png"} 
+                        description={"Criação  de um aplicativo web de videoconferência com chat, compartilhamento de tela, e sistema de transferência de arquivos, usando HTML, CSS e Java-script no Front-End e NodeJs no Back-End  para a startup Hiper School, que está desenvolvendo, uma plataforma de aulas particulares online."} 
+                        linkRepository={"https://github.com/JCARSAN/VideoConferenciaApp"} 
+                />
+                <PortfolioWork 
+                        title={"Upload de Imagens"} 
+                        src={"/images/upfi.png"} 
+                        description={"Criação de um app, como exercício do curso de ReactJs da Rockeatseat, para salvar imagens do computador em uma nuvem, usando ChackraUI, NextJS, TypeScript e FaunaDB."} 
+                        linkRepository={"https://github.com/JCARSAN/Upload_de_Imagens"} 
+                />
+                <PortfolioWork 
+                        title={"Blog Spacetravelling"} 
+                        src={"/images/spacetravellingproject.png"} 
+                        description={"Criação de um blog feito com NextJS, o qual é conectado a API do CMS Prismic, onde são acessados os posts do blog e é aplicado o conceito de paginação. A estilização do site foi feita usando Sasss.  Foi realizado como exercício no curso de React da Rockeatseat."} 
+                        linkRepository={"https://github.com/JCARSAN/blog-react-rocketseat"} 
+                />
+            */}
             </Flex>
         </VStack>
     )
